7e51ba2e49d4b40ecc2a3d47cc5af3515e2b033c
2train395 <2train395@gmail.com>
2016-01-27 23:01:43 -0500

    Updated with some AUTON

--- Stronghold_Test_Drive/src/org/usfirst/frc/team395/robot/Robot.java
+++ Stronghold_Test_Drive/src/org/usfirst/frc/team395/robot/Robot.java
@@ -10,6 +10,7 @@ package org.usfirst.frc.team395.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive; 
 import edu.wpi.first.wpilibj.RobotDrive.MotorType;
@@ -23,7 +24,7 @@ import edu.wpi.first.wpilibj.RobotDrive.MotorType;
  */
 public class Robot extends IterativeRobot {
    
-	// DRIVE
+	//DRIVE
 	RobotDrive robotDrive;
 	final double DRIVE_FACTOR = 0.5;
 	final int frontLeftChannel	= 1;
@@ -31,28 +32,51 @@ public class Robot extends IterativeRobot {
 	final int frontRightChannel	= 3;
 	final int rearRightChannel	= 4;
 	
-	// JOYSTICKS
+	//JOYSTICKS
 	Joystick driveStick;
-	final int driveStickChannel = 1;
+	final int DRIVE_STICK_CHANNEL = 1;
 	
+	//XBOX
+	Joystick xboxController;
+	final int XBOX_CONTROLLER_CHANNEL = 2;
+	final int ROLLER_IN_AXIS = 2;
+	final int ROLLER_OUT_AXIS = 3;
+	
+	//ROLLER
+	Talon leftRoller;
+	Talon rightRoller;
+	final int LEFT_ROLLER_CHANNEL = 5;
+	final int RIGHT_ROLLER_CHANNEL = 6;
+	
+	//AUTON
+	int autonStage = 1;
+	Timer autonTimer;
+	boolean sequenceComplete;
+
     public void robotInit() {
     
-    //DRIVE
-    robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
-    robotDrive.setExpiration(0.1); 
-	robotDrive.setSafetyEnabled(true); 
+    	//DRIVE
+    	robotDrive = new RobotDrive(frontLeftChannel, rearLeftChannel, frontRightChannel, rearRightChannel);
+    	robotDrive.setExpiration(0.1); 
+    	robotDrive.setSafetyEnabled(true); 
     
-	// JOYSTICK
-	driveStick = new Joystick(driveStickChannel);
-    }
-    
-    public void autonomousInit() {
-  
+    	//ROLLER
+    	leftRoller = new Talon(LEFT_ROLLER_CHANNEL);
+    	rightRoller = new Talon(RIGHT_ROLLER_CHANNEL);
+    	
+    	//JOYSTICK & XBOX
+    	driveStick = new Joystick(DRIVE_STICK_CHANNEL);
+    	xboxController = new Joystick(XBOX_CONTROLLER_CHANNEL);
+	
+    	//AUTON
+    	autonTimer = new Timer();
+    	sequenceComplete = false;
     }
 
     /**
      * This function is called periodically during autonomous
      */
+    
     public void autonomousPeriodic() {
     	
     }
@@ -63,7 +87,24 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         
     	manualDrive();
-    
+    	
+    	if(xboxController.getRawAxis(ROLLER_IN_AXIS) > 0.5){
+    		rightRoller.set(1.0);
+    		leftRoller.set(1.0);
+    	}
+    	else{
+    		rightRoller.set(0.0);
+    		leftRoller.set(0.0);
+    	}
+    	
+    	if(xboxController.getRawAxis(ROLLER_OUT_AXIS) > 0.5){
+    		rightRoller.set(-1.0);
+    		leftRoller.set(-1.0);
+    	}
+    	else{
+    		rightRoller.set(0.0);
+    		leftRoller.set(0.0);
+    	}
     }
     
     /**
