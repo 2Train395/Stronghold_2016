a79f1c2035c07814f4b9e2108bb2b6e5353f2d32
2train395 <2train395@gmail.com>
2016-02-11 15:09:00 -0500

    C3PO broke :(  BACKUP
    requiescat in pace

--- Stronghold_Test_Drive/src/org/usfirst/frc/team395/robot/Robot.java
+++ Stronghold_Test_Drive/src/org/usfirst/frc/team395/robot/Robot.java
@@ -28,14 +28,10 @@ public class Robot extends IterativeRobot {
    
 	// DRIVE
 	RobotDrive robotDrive;
-<<<<<<< HEAD
-	double DRIVE_FACTOR = 0.5;
-	double ROTATE_FACTOR = 0.9;
 	double ARM_SPEED = 1.0;
-=======
 	final double DRIVE_FACTOR = 0.5;					//----TEST
 	final double ROTATE_FACTOR = 0.85;					//----TEST
->>>>>>> origin/master
+
 	final int frontLeftChannel	= 4;
 	final int rearLeftChannel	= 3;
 	final int frontRightChannel	= 1;
@@ -47,7 +43,7 @@ public class Robot extends IterativeRobot {
 	Joystick xboxController;
 	final int driveStickChannel = 1;
 	final int XBOX_CONTROLLER_CHANNEL = 2;
-<<<<<<< HEAD
+
 	final int ROLLER_INXB = 6;
 	final int ROLLER_OUTXB = 5;
 	final int ROLLER_INJS = 1;
@@ -55,11 +51,10 @@ public class Robot extends IterativeRobot {
 	final int ARM_UP = 4;
 	final int ARM_DOWN = 1;
 
-=======
+
 	final int ROLLER_OUT = 5;
 	final int ROLLER_IN = 6;
 	
->>>>>>> origin/master
 	// ROLLER
 	Talon roller;
 	final int ROLLER_CHANNEL = 5;
@@ -73,17 +68,16 @@ public class Robot extends IterativeRobot {
 	double REVERSE_ARM = 1;
 		
 	// AUTONOMOUS
-<<<<<<< HEAD
 	double autonMove;
 	double autonRotateSpeed;
-=======
->>>>>>> origin/master
 	int autonStage = 1;
 	Timer autonTimer;
-<<<<<<< HEAD
 	final double STOP_TIME = 1.00;
 	final double MOVE_TIME = 2.15;				// TEST BEFORE USING!!!
 	final double RELEASE_TIME = 1.0;
+	final int AUTON_MODE = 1;
+	double autonMoveSpeed;
+	
 	
 	//ANALOG 
 	AnalogGyro gyro;
@@ -91,14 +85,8 @@ public class Robot extends IterativeRobot {
 	final double GYRO_SENSITIVITY = 0.007;
 	final int TEMP_CHANNEL = 1;
 	final double GYRO_CORRECTION = 0.03;
-=======
-	final int AUTON_MODE = 1;
-	double autonMoveSpeed;
-	double autonRotateSpeed;
-	final double STOP_TIME = 1.00;
-	final double MOVE_TIME = 2.15;				// TEST BEFORE USING!!!
-	final double RELEASE_TIME = 1.0;
->>>>>>> origin/master
+	
+
 	
     public void robotInit() {
     
@@ -139,7 +127,7 @@ public class Robot extends IterativeRobot {
 	        	
 	   		autonTimer.reset();
 	   		autonTimer.start();
-<<<<<<< HEAD
+
 	    	gyro.reset();
 	    	
 	   		while(autonTimer.get() < MOVE_TIME){	
@@ -210,7 +198,7 @@ public class Robot extends IterativeRobot {
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMove, autonRotateSpeed);
 	    	}
-=======
+
 	    	
 	   		while(autonTimer.get() < MOVE_TIME){	
 	    		
@@ -218,7 +206,7 @@ public class Robot extends IterativeRobot {
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMoveSpeed, autonRotateSpeed);
 	    	}
->>>>>>> origin/master
+
 	    	
 	   		autonStage = 2;
 	    	autonTimer.stop();
@@ -244,15 +232,12 @@ public class Robot extends IterativeRobot {
 	    	
 	   		while(autonTimer.get() < MOVE_TIME){	
 	    	
-<<<<<<< HEAD
 	   			autonMove = -0.9;
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMove, autonRotateSpeed);
-=======
 	   			autonMoveSpeed = -0.9;
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMoveSpeed, autonRotateSpeed);
->>>>>>> origin/master
 	    	}
 	    	
 	    	autonStage = 4;
@@ -271,11 +256,8 @@ public class Robot extends IterativeRobot {
 	    	
 	    	}
 	    }
-<<<<<<< HEAD
-
-=======
 	    
-	    if (AUTON_MODE == 2){
+	    if (AUTON_MODE == 3){
 
 	    	if(autonStage==1){
 	        	
@@ -335,7 +317,6 @@ public class Robot extends IterativeRobot {
 	    	}
 	    }
 
->>>>>>> origin/master
 	    if (AUTON_MODE == 3){
 	    	
 	    	if(autonStage==1){
@@ -345,15 +326,14 @@ public class Robot extends IterativeRobot {
 	    	
 	   		while(autonTimer.get() < MOVE_TIME){	
 	    		
-<<<<<<< HEAD
 	   			autonMove = 0.9;
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMove, autonRotateSpeed);
-=======
+
 	   			autonMoveSpeed = 0.9;
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMoveSpeed, autonRotateSpeed);
->>>>>>> origin/master
+
 	    	}
 	    	
 	   		autonStage = 2;
@@ -380,15 +360,12 @@ public class Robot extends IterativeRobot {
 	    	
 	   		while(autonTimer.get() < MOVE_TIME){	
 	    	
-<<<<<<< HEAD
 	   			autonMove = -0.9;
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMove, autonRotateSpeed);
-=======
 	   			autonMoveSpeed = -0.9;
 	    		autonRotateSpeed = 0.0;
 	    		robotDrive.arcadeDrive(autonMoveSpeed, autonRotateSpeed);
->>>>>>> origin/master
 	    	}
 	    	
 	    	autonStage = 4;
@@ -432,19 +409,16 @@ public class Robot extends IterativeRobot {
     	
     	double driveValue = driveStick.getY();	
     		
-<<<<<<< HEAD
     	robotDrive.arcadeDrive(driveValue , rotateValue * ROTATE_FACTOR);
 
     	SmartDashboard.putNumber("gyro", gyro.getAngle());
 
-=======
     		rotateValue = driveStick.getX();
-    	}
   	
     //	driveValue *= DRIVE_FACTOR;
     	rotateValue *= ROTATE_FACTOR;
     	robotDrive.arcadeDrive(driveValue, rotateValue);
->>>>>>> origin/master
+
     }
     
     public void rollerControl(){	
